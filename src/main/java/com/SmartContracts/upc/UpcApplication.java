c6683ad52f1b2dffff2cdbc444d89fa30d7437f3
FabrizCastro <fabrizziocastro2612@gmail.com>
2024-10-31 00:47:45 -0500

    Update CorsConfig

--- src/main/java/com/SmartContracts/upc/UpcApplication.java
+++ src/main/java/com/SmartContracts/upc/UpcApplication.java
@@ -17,11 +17,10 @@ public class UpcApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
+				registry.addMapping("/api/smartcontract/v1/**") // Aplica a todas las rutas
 						.allowedOrigins("*") // URL de tu frontend
-						.allowedMethods("GET", "POST", "PUT", "DELETE") // Especifícalos explícitamente
-						.allowedHeaders("*"); // Permitir cookies si es necesario
-
+						.allowedMethods("*")
+						.allowedHeaders("*");
 			}
 		};
 	}
